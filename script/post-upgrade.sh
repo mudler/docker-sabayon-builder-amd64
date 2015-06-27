47938c4935664345a9e316cd37553618d988fa18
mudler <mudler@dark-lab.net>
2015-06-27 12:12:39 +0200

    Revert "Adding package: sudo"
    This reverts commit ff034ea21152a1241f97a099afd3b99f4e73900f.

--- script/post-upgrade.sh
+++ script/post-upgrade.sh
@@ -71,7 +71,6 @@ PACKAGES_TO_ADD=(
     "net-misc/rsync"
     "app-crypt/gnupg"
     "sys-devel/gcc"
-    "app-admin/sudo"
 )
 
 rsync -av -H -A -X --delete-during "rsync://rsync.at.gentoo.org/gentoo-portage/licenses/" "/usr/portage/licenses/"
